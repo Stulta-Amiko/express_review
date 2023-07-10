42d870df4186ea129fa408a5e7c59371641a2a82
Stulta-Amiko <phh01010312@gmail.com>
2023-07-10 20:29:23 +0900

    fix app.js

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ const dbConnection = process.env.DB_CONNECT
 
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    )
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE')
+
+    next()
+})
+
 app.use('/api/places', placesRouter)
 
 app.use('/api/users', usersRouter)
