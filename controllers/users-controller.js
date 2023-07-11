948c883b540df5f225c4dacd143a29a7b73b9eac
Stulta-Amiko <phh01010312@gmail.com>
2023-07-11 12:21:10 +0900

    fix user-controller add login user

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -78,7 +78,10 @@ const login = async(req, res, next) => {
         return next(error)
     }
 
-    res.json({ message: 'Logged in' })
+    res.json({
+        message: 'Logged in',
+        user: existingUser.toObject({ getters: true }),
+    })
 }
 
 export default { getUsers, signup, login }
